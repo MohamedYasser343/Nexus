ec19dc970f015440819d08be352b58c47a178699
AdhamHaithamEid <adhamhaithameid@gmail.com>
2024-02-08 20:31:57 +0200

    basic sign up page design

--- Frontend/Mobile_App/nexus/lib/signup.dart
+++ Frontend/Mobile_App/nexus/lib/signup.dart
@@ -9,6 +9,34 @@ class SignupPage extends StatefulWidget {
 class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Color(0xFF747EF5),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back_ios,
+          size: 35,
+          color: Color(0xFF3B3084),
+          )
+          ),
+      ),
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          color: Color(0xFF747EF5),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+
+            ],
+          ),
+        )
+      ),
+    );
   }
 }
