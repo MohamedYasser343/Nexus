0d5d5978b7a0b78ad6506077a48cba0c5b1ee3b9
AdhamHaithamEid <adhamhaithameid@gmail.com>
2024-02-08 20:31:12 +0200

    basic login page design

--- Frontend/Mobile_App/nexus/lib/Login.dart
+++ Frontend/Mobile_App/nexus/lib/Login.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -9,6 +11,34 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Color(0xFF747EF5),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back_ios,
+          size: 35,
+          color: Color(0xFF3B3084),
+          )
+          ),
+      ),
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          color: Color(0xFF747EF5),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+
+            ],
+          ),
+        )
+      ),
+    );
   }
 }
