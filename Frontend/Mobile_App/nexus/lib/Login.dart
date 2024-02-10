fba6ab25afff5d510af741ab4967ef96d9a246a3
AdhamHaithamEid <adhamhaithameid@gmail.com>
2024-02-10 19:25:16 +0200

    made a base in signup page
    (copying the login page)

--- Frontend/Mobile_App/nexus/lib/Login.dart
+++ Frontend/Mobile_App/nexus/lib/Login.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 import 'package:Nexus/signup.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 class LoginPage extends StatefulWidget {
   @override
   State<LoginPage> createState() => _LoginPageState();
