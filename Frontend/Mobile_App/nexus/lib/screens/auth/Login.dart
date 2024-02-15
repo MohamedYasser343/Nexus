9c6c5259ce3723bcd9aa0c0cc550eb88d75b3edb
AdhamHaithamEid <adhamhaithameid@gmail.com>
2024-02-15 20:33:51 +0200

    .

--- Frontend/Mobile_App/nexus/lib/screens/auth/Login.dart
+++ Frontend/Mobile_App/nexus/lib/screens/auth/Login.dart
@@ -1,10 +1,9 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-import 'package:Nexus/signup.dart';
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace, use_key_in_widget_constructors, file_names
+import 'package:Nexus/screens/auth/signup.dart';
 import 'package:flutter/material.dart';
 class LoginPage extends StatefulWidget {
   @override
-  State<LoginPage> createState() => _LoginPageState();
-}
+  State<LoginPage> createState() => _LoginPageState();}
 class _LoginPageState extends State<LoginPage> {
   bool passwordVisible = true;
   final _formKey = GlobalKey<FormState>();
@@ -82,7 +81,6 @@ class _LoginPageState extends State<LoginPage> {
                                 // All fields are valid, perform login
                                 // Add your login logic here
                               } else {
-                                // Validation failed, show error message
                                 ScaffoldMessenger.of(context).showSnackBar(
                                   SnackBar(
                                     content: Text('Please fill in all fields'),
