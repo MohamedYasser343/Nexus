bcef2b8cdf515d1ff7db59daefc23748a0f3ad39
AdhamHaithamEid <adhamhaithameid@gmail.com>
2024-02-21 07:14:45 +0200

    fixed the form key availability in login page

--- Frontend/Mobile_App/nexus/lib/screens/home/home.dart
+++ Frontend/Mobile_App/nexus/lib/screens/home/home.dart
@@ -8,14 +8,29 @@ class home extends StatefulWidget {
 class _homeState extends State<home> {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Positioned.fill(
-          child: Container(
-            color: Color(0xFF747EF5),
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Color(0xFF747EF5),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back_ios_rounded,
+            size: 35,
+            color: Color(0xFF3B3084),
           ),
         ),
-      ],
+      ),
+      body:Container(
+        padding: EdgeInsets.only(top: 60),
+        color: Color(0xFF747EF5),
+        child: Stack(
+
+        )
+        )
     );
   }
 }
